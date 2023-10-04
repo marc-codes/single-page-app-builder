256ce94c2435ca41b4576c9b7f5cdcbdfa9f33ae
Dipanshu Tihal <tihal.dipanshu@gmail.com>
2023-10-04 21:43:32 +0530

    Added User and Roles Feature

--- HomeCinema.Data/Configurations/UserRoleConfiguration.cs
+++ HomeCinema.Data/Configurations/UserRoleConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using HomeCinema.Entities;
+﻿﻿using HomeCinema.Entities;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity.ModelConfiguration;
@@ -8,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace HomeCinema.Data.Configurations
 {
-    public class UserRoleConfiguration
+    public class UserRoleConfiguration : EntityBaseConfiguration<UserRole>
     {
+        public UserRoleConfiguration()
+        {
+            Property(ur => ur.UserId).IsRequired();
+            Property(ur => ur.RoleId).IsRequired();
+        }
     }
 }
